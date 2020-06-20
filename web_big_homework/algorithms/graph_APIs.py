523a0664943970f3906f123f1efee7d71d2ff618
lupeng <lup19@mails.tsinghua.edu.cn>
2020-06-19 23:49:50 -0700

    a little change

--- web_big_homework/algorithms/graph_APIs.py
+++ web_big_homework/algorithms/graph_APIs.py
@@ -3,7 +3,8 @@ from create_graph import GRAPH_PATH
 
 class CVGraph:
 	def __init__(self):
-		self.graph = self.retrive_and_create_graph(GRAPH_PATH)
+		self.graph_path = GRAPH_PATH
+		self.graph = self.retrive_and_create_graph(self.graph_path)
 
 	def test(self):
 		print(len(self.graph.nodes))
--- web_big_homework/graph/views.py
+++ web_big_homework/graph/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render, redirect
-
+from algorithms import graph_APIs
 # Create your views here.
 def home_page(request):
 	return render(request, 'home.html')
